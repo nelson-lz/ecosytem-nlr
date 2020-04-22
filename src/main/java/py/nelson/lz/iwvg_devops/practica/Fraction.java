e9682497bc60c3a32091c0b626e9285b4b254df3
nelson-lz <nelsonlz@outlook.com>
2020-04-22 14:12:01 -0400

    Add thrid feature in class Fraction

--- src/main/java/py/nelson/lz/iwvg_devops/practica/Fraction.java
+++ src/main/java/py/nelson/lz/iwvg_devops/practica/Fraction.java
@@ -25,6 +25,7 @@ package py.nelson.lz.iwvg_devops.practica;
  */
 //se ha agregado una nueva caracteristica xD
     //Segunda mejora a la clase xD
+    // tercera mejora a fraction
 public class Fraction {
 
     private int numerator;
